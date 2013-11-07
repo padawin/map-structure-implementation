e96252127bdc7d368972252a223c6fe0bd0bce77
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-11-07 14:54:05 +0000

    tests added for an unexisting value

--- test.c
+++ test.c
@@ -7,6 +7,7 @@ int main()
 	map m;
 	int items_number = 10;
 	map_item items[items_number];
+	char *unknown;
 
 	char *keys[] = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj"};
 	char *values[] = {"foo", "bar", "toto", "tata", "something", "10", "truc", "3.14", "moi", "vous"};
@@ -24,9 +25,14 @@ int main()
 	m.items_number = items_number;
 
 	printf("%s\n", (char *) map_get_entry("aaa", &m));
-	printf("%s %s\n", m.items[0].key, (char *) m.items[0].item);
-	printf("%s\n", (char *) map_get_entry("bbb", &m));
-	printf("%s\n", (char *) map_get_entry("uuu", &m));
+	printf("%s\n", (char *) map_get_entry("iii", &m));
+	unknown = (char *) map_get_entry("uuu", &m);
+	if (unknown != NULL) {
+		printf("%s\n", unknown);
+	}
+	else {
+		printf("'uuu' is not in the map\n");
+	}
 
 	return 0;
 }
