30eb86562720cb7aed228e57785f1eb69b0ce73a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-11-07 14:55:02 +0000

    test added for the upade of an element

--- test.c
+++ test.c
@@ -8,6 +8,7 @@ int main()
 	int items_number = 10;
 	map_item items[items_number];
 	char *unknown;
+	int result;
 
 	char *keys[] = {"aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg", "hhh", "iii", "jjj"};
 	char *values[] = {"foo", "bar", "toto", "tata", "something", "10", "truc", "3.14", "moi", "vous"};
@@ -33,6 +34,11 @@ int main()
 	else {
 		printf("'uuu' is not in the map\n");
 	}
+
+	printf("before, for the key 'ddd', the value was '%s'\n", (char *) map_get_entry("ddd", &m));
+	result = map_add_entry("ddd", "new ddd", &m);
+	printf("and after, for the key 'ddd', the value is '%s'\n", (char *) map_get_entry("ddd", &m));
+
 
 	return 0;
 }
